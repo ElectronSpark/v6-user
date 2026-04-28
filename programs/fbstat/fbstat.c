f0d71342e6bcc81780282ecf5a711af9247acb3c
electronspark <electronsparkle@gmail.com>
2026-04-28 12:30:24 -0400

    user: add framebuffer stats tool

---
+++ programs/fbstat/fbstat.c
@@ -0,0 +1,34 @@
+#include "kernel/inc/types.h"
+#include "kernel/inc/dev/fb.h"
+#include "kernel/inc/uabi/fcntl.h"
+#include "user/user.h"
+
+int main(int argc, char *argv[])
+{
+    struct fb_gpu_stats stats;
+    int fd;
+
+    (void)argc;
+    (void)argv;
+
+    fd = open("/dev/fb0", O_RDONLY);
+    if (fd < 0) {
+        fprintf(2, "fbstat: open /dev/fb0 failed\n");
+        return 1;
+    }
+    if (ioctl(fd, FB_GPU_GET_STATS, &stats) < 0) {
+        fprintf(2, "fbstat: FB_GPU_GET_STATS failed\n");
+        close(fd);
+        return 1;
+    }
+
+    printf("full_blits %lu\n", stats.full_blits);
+    printf("partial_blits %lu\n", stats.partial_blits);
+    printf("clipped_blits %lu\n", stats.clipped_blits);
+    printf("rejected_blits %lu\n", stats.rejected_blits);
+    printf("fill_rects %lu\n", stats.fill_rects);
+    printf("copy_rects %lu\n", stats.copy_rects);
+    printf("blit_bytes %lu\n", stats.blit_bytes);
+    close(fd);
+    return 0;
+}
