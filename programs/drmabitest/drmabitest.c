8b4985916bfe5c71784f999ce97958df20f9a5ad
electronspark <electronsparkle@gmail.com>
2026-06-06 16:47:44 -0400

    Verify PRIME import shares pixels across owners

--- programs/drmabitest/drmabitest.c
+++ programs/drmabitest/drmabitest.c
@@ -1059,17 +1059,39 @@ static void probe_dumb_bo(struct drm_node *node)
     prime.handle = create.handle;
     ret = call_ioctl(node->fd, DRM_IOCTL_PRIME_HANDLE_TO_FD, &prime);
     if (ret == 0 && prime.fd >= 0) {
+        uint32 *parent_pixels;
+        uint32 parent_first = 0;
+        uint32 parent_last = 0;
+        uint32 pattern_first = 0xff224466;
+        uint32 pattern_last = 0xff6688aa;
         int child_status = -1;
         int child = fork();
+
+        memset(&map, 0, sizeof(map));
+        map.handle = create.handle;
+        ret = call_ioctl(node->fd, DRM_IOCTL_MODE_MAP_DUMB, &map);
+        parent_pixels = ret == 0 ?
+            mmap(0, (int)create.size, PROT_READ | PROT_WRITE, MAP_SHARED,
+                 node->fd, map.offset) : MAP_FAILED;
+        if (parent_pixels != MAP_FAILED) {
+            parent_pixels[0] = pattern_first;
+            parent_pixels[(create.height - 1) * (create.pitch / 4) +
+                          (create.width - 1)] = pattern_last;
+        }
 
         if (child == 0) {
             int child_fd = open(node->path, 0);
             struct drm_mode_create_dumb_compat child_dummy;
+            struct drm_mode_map_dumb_compat child_map;
             struct drm_prime_handle_compat child_import;
             struct drm_mode_destroy_dumb_compat child_destroy;
             struct drm_gem_close_compat child_close;
+            uint32 *child_pixels = MAP_FAILED;
+            uint32 child_first = 0;
+            uint32 child_last = 0;
             int dummy_ret;
             int import_ret;
+            int map_ret = -999;
             int close_ret = -999;
             int destroy_ret = -999;
 
@@ -1086,6 +1108,25 @@ static void probe_dumb_bo(struct drm_node *node)
                 call_ioctl(child_fd, DRM_IOCTL_PRIME_FD_TO_HANDLE,
                            &child_import) : -EBADF;
             if (import_ret == 0) {
+                memset(&child_map, 0, sizeof(child_map));
+                child_map.handle = child_import.handle;
+                map_ret = call_ioctl(child_fd, DRM_IOCTL_MODE_MAP_DUMB,
+                                     &child_map);
+                if (map_ret == 0) {
+                    child_pixels = mmap(0, (int)create.size,
+                                        PROT_READ | PROT_WRITE, MAP_SHARED,
+                                        child_fd, child_map.offset);
+                    if (child_pixels != MAP_FAILED) {
+                        child_first = child_pixels[0];
+                        child_last =
+                            child_pixels[(create.height - 1) *
+                                         (create.pitch / 4) +
+                                         (create.width - 1)];
+                        child_pixels[1] = 0xffabcdef;
+                    }
+                }
+            }
+            if (import_ret == 0) {
                 memset(&child_close, 0, sizeof(child_close));
                 child_close.handle = child_import.handle;
                 close_ret = call_ioctl(child_fd, DRM_IOCTL_GEM_CLOSE,
@@ -1101,27 +1142,42 @@ static void probe_dumb_bo(struct drm_node *node)
             printf("%s:DRM_IOCTL_PRIME_FD_TO_HANDLE.cross_owner.child: "
                    "open=%d dummy=%d dummy_handle=%u import=%d errno=%d "
                    "child_handle=%u parent_handle=%u different=%u "
-                   "close=%d close_errno=%d destroy=%d destroy_errno=%d\n",
+                   "map=%d map_errno=%d same_pixels=%u child_first=0x%x "
+                   "child_last=0x%x close=%d close_errno=%d destroy=%d "
+                   "destroy_errno=%d\n",
                    node->name, child_fd, dummy_ret, child_dummy.handle,
                    import_ret, saved_errno(import_ret), child_import.handle,
                    create.handle, child_import.handle != create.handle,
-                   close_ret, saved_errno(close_ret), destroy_ret,
-                   saved_errno(destroy_ret));
+                   map_ret, saved_errno(map_ret),
+                   child_first == pattern_first && child_last == pattern_last,
+                   child_first, child_last, close_ret, saved_errno(close_ret),
+                   destroy_ret, saved_errno(destroy_ret));
+            if (child_pixels != MAP_FAILED)
+                munmap((void *)child_pixels, (int)create.size);
             if (child_fd >= 0)
                 close(child_fd);
             exit(dummy_ret == 0 && import_ret == 0 && close_ret == 0 &&
                  destroy_ret == 0 && child_import.handle != 0 &&
-                 child_import.handle != create.handle ? 0 : 1);
+                 child_import.handle != create.handle && map_ret == 0 &&
+                 child_first == pattern_first &&
+                 child_last == pattern_last ? 0 : 1);
         }
         if (child > 0)
             wait(&child_status);
 
+        if (parent_pixels != MAP_FAILED) {
+            parent_first = parent_pixels[0];
+            parent_last = parent_pixels[1];
+            munmap((void *)parent_pixels, (int)create.size);
+        }
         memset(&map, 0, sizeof(map));
         map.handle = create.handle;
         ret = call_ioctl(node->fd, DRM_IOCTL_MODE_MAP_DUMB, &map);
         printf("%s:DRM_IOCTL_PRIME_FD_TO_HANDLE.cross_owner.parent: "
-               "child_status=%d map=%d errno=%d offset=%lu\n",
-               node->name, child_status, ret, saved_errno(ret), map.offset);
+               "child_status=%d map=%d errno=%d offset=%lu "
+               "parent_first=0x%x child_write=0x%x\n",
+               node->name, child_status, ret, saved_errno(ret), map.offset,
+               parent_first, parent_last);
         close(prime.fd);
     }
 
